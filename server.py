60ae9d1385da176bbb407df0956f50e98918ece5
Ignacio Carrera <carrera.ignacio05@gmail.com>
2025-05-09 12:42:32 -0300

    Update server.py

--- server.py
+++ server.py
@@ -34,4 +34,4 @@ def ver():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)p.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
