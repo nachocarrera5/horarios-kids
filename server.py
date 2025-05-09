ce8db3634b56d1e792a3b1d4c676300e1dbf0309
Ignacio Carrera <carrera.ignacio05@gmail.com>
2025-05-09 12:39:03 -0300

    Update server.py

--- server.py
+++ server.py
@@ -32,4 +32,6 @@ def ver():
     return jsonify(respuestas)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)p.run(debug=True)
